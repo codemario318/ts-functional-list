8592d34c71603de2defddbdb3bccd2829238fc90
mario <codemario318@gmail.com>
2024-07-31 10:02:50 +0900

    [ADD] TDD 준비

---
+++ tests/FunctionalLinkedList.spec.ts
@@ -0,0 +1,20 @@
+import {FunctionalLinkedList} from "../src/FunctionalLinkedList";
+
+describe('ObjectOrientedLinkedList', () => {
+  let list: FunctionalLinkedList<number>;
+
+  beforeEach(() => {
+    list = new FunctionalLinkedList<number>();
+  });
+
+  describe('push', () => {});
+  describe('delete', () => {});
+  describe('at', () => {});
+  describe('find', () => {});
+  describe('some', () => {});
+  describe('forEach', () => {});
+  describe('map', () => {});
+  describe('filter', () => {});
+  describe('isEmpty()', () => {});
+  describe('size()', () => {});
+});
---
+++ tests/ObjectOrientedLinkedList.spec.ts
@@ -0,0 +1,20 @@
+import {ObjectOrientedLinkedList} from "../src/ObjectOrientedLinkedList";
+
+describe('ObjectOrientedLinkedList', () => {
+  let list: ObjectOrientedLinkedList<number>;
+
+  beforeEach(() => {
+    list = new ObjectOrientedLinkedList<number>();
+  });
+
+  describe('push', () => {});
+  describe('delete', () => {});
+  describe('at', () => {});
+  describe('find', () => {});
+  describe('some', () => {});
+  describe('forEach', () => {});
+  describe('map', () => {});
+  describe('filter', () => {});
+  describe('isEmpty()', () => {});
+  describe('size()', () => {});
+});
